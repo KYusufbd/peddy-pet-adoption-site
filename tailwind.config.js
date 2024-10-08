43e69b39d6853d81af024b63d5101db0fab5a936
Kazi Yusuf <yfaka001@gmail.com>
2024-10-08 10:17:09 +0600

    Fixed bug in loading font files (ttf)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    fontFamily: {
+      'sans': ['Lato', ...defaultTheme.fontFamily.sans]
+    },
     extend: {},
   },
   plugins: [
