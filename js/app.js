efbd796d5d37e714a37903e0253a508561db48a9
Kazi Yusuf <yfaka001@gmail.com>
2024-10-09 16:12:59 +0600

    Pet list is displayed well. Now time to active category buttons.

--- js/app.js
+++ js/app.js
@@ -9,15 +9,7 @@ const getData = async (api) => {
 getData("https://openapi.programming-hero.com/api/peddy/categories").then(
   (res) => {
     const categorySection = document.getElementById("categories");
-    console.log(res); // Just for testing perpose
     res.categories.map((e) => {
-        // const button = categorySection.appendChild(document.createElement('button'));
-        // button.innerHTML = `
-        //     <img src="${e.category_icon}" />
-        //     ${e.category}
-        //     `;
-        // button.classList.add('btn', 'btn-outline')
-    
       const catElements = categorySection.innerHTML;
       categorySection.innerHTML =
         catElements +
@@ -30,3 +22,68 @@ getData("https://openapi.programming-hero.com/api/peddy/categories").then(
     });
   }
 );
+
+// Display all available pets / pets of a perticular category.
+function displayPets(source = "pets") {
+  getData(`https://openapi.programming-hero.com/api/peddy/${source}`).then(
+    (res) => {
+      let data;
+      res.pets ? (data = res.pets) : (data = res.data);
+      data.map((e) => {
+        const petDisplay = document.getElementById("pet-display");
+        const prevCards = petDisplay.innerHTML;
+        petDisplay.innerHTML =
+          prevCards +
+          `
+            <div class="flex flex-col p-5 gap-3 shadow-lg rounded-lg">
+              <div class="rounded-lg w-full h-40 overflow-hidden">
+                <img
+                    src="${e.image}"
+                    alt=""
+                    class="h-full min-w-full rounded-lg"
+                />
+              </div>
+              <div class="flex flex-col gap-3">
+                <h6 class="font-bold">${e.pet_name}</h6>
+                <div class="flex flex-row gap-1">
+                  <span>${breedSvg}</span>
+                  <p>Breed: ${e.breed ? e.breed : "Not available"}</p>
+                </div>
+                <div class="flex flex-row gap-1">
+                <span>${birthSvg}</span>
+                  <p>Birth: ${e.date_of_birth ? e.date_of_birth : "Not available"}</p>
+                </div>
+                <div class="flex flex-row gap-1">
+                <span>${genderSvg}</span>
+                  <p>Gender: ${e.gender ? e.gender : "Not available"}</p>
+                </div>
+                <div class="flex flex-row gap-1">
+                  <span>${priceSvg}</span>
+                  <p>Price : ${e.price}$</p>
+                </div>
+              </div>
+            </div>
+        `;
+      });
+    }
+  );
+}
+
+// displayPets("category/cat");
+displayPets();
+
+/* 
+    Pet object model:
+{
+    "petId": 1,
+    "breed": "Golden Retriever",
+    "category": "Dog",
+    "date_of_birth": "2023-01-15",
+    "price": 1200,
+    "image": "https://i.ibb.co.com/p0w744T/pet-1.jpg",
+    "gender": "Male",
+    "pet_details": "This friendly male Golden Retriever is energetic and loyal, making him a perfect companion for families. Born on January 15, 2023, he enjoys playing outdoors and is especially great with children. Fully vaccinated, he's ready to join your family and bring endless joy. Priced at $1200, he offers love, loyalty, and a lively spirit for those seeking a playful yet gentle dog.",
+    "vaccinated_status": "Fully",
+    "pet_name": "Sunny"
+}
+*/
